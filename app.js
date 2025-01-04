5aea3dce7f0f9533a96b9a4b3a19b4adea1f3a97
CodewithMK180105 <manishkumar180105@gmail.com>
2025-01-04 19:31:57 +0530

    Implemented Create, Read, Delete operations

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express= require('express')
 const app=express()
 
 const path=require('path');
+const userModel=require('./models/user.models');
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -11,8 +12,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req,res)=>{
     res.render("index");
 })
-app.get('/read', (req,res)=>{
-    res.render("read");
+app.get('/read', async (req,res)=>{
+    let users= await userModel.find();
+    res.render("read", {users});
+})
+app.get('/delete/:id', async (req,res)=>{
+    let user= await userModel.findOneAndDelete({_id: req.params.id});
+    res.redirect("/read");
+})
+app.post('/create', async (req,res)=>{
+    let {name, email, imageUrl}= req.body;
+    let createdUser=await userModel.create({
+        name,      // name: name
+        email,     // email: email
+        imageUrl   // imageUrl: imageUrl
+    })
+    res.redirect("/read");
 })
 
 app.listen(3000);
